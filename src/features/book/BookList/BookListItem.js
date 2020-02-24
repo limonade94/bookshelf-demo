26c6e4bbe888717b0c2bd994ae28c2943e60cb08
Phan Chau <pc94200@gmail.com>
2020-02-24 07:05:06 +0100

    Restyle bookList

--- src/features/book/BookList/BookListItem.js
+++ src/features/book/BookList/BookListItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Item, Icon, Button, List } from 'semantic-ui-react';
-import BookListRelated from './BookListRelated';
+import { Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class BookListItem extends Component {
@@ -9,62 +8,33 @@ class BookListItem extends Component {
     const { book, deleteBook } = this.props;
 
     return (
-      <Segment.Group>
-        <Segment>
-          <Item.Group>
-            <Item>
-              <Item.Content>
-                <Item.Image size="small" src={book.photoUrl} />       
-                <Button
-                  as={Link}
-                  to={`/books/${book.id}`}
-                  color="teal" 
-                  content="Learn more" 
-                />
-                <Button 
-                  onClick={() => deleteBook(book.id)}
-                  color="grey" 
-                  content="Delete"
-                />
-              </Item.Content>                      
-              <Item.Content>
-                <Item.Header as="a">{book.title}</Item.Header>
-                <Item.Description>
-                  <Icon name="user circle" />
-                  {book.author}
-                </Item.Description>
-                <Item.Description>
-                  <Icon name="calendar" />
-                  {book.date}
-                </Item.Description>
-                <Item.Description>
-                  <Icon name="file text" />
-                  {book.page} pages
-                </Item.Description>
-                <Item.Description>
-                  <Icon name={book.category === 'React' ? 'react' : 'js'} />
-                  {book.category}
-                </Item.Description>
-                <Item.Description>
-                  ISBN : {book.isbn}
-                </Item.Description>
-                <Item.Description className="last">
-                  {book.accept}
-                </Item.Description>                
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Segment> 
-        {/* <Segment>
-          <List horizontal>
-            {
-              book.relatedItem && book.relatedItem.map(item => (
-                <BookListRelated key={item.title} item={item} />
-              ))
-            }          
-          </List>
-        </Segment> */}
-      </Segment.Group>
+      <section className="book-item flex-container">
+        <img src={book.photoUrl} alt={book.title} />
+        
+        <ul>
+          <li>{book.title}</li>
+          <li><Icon name="user circle" />{book.author}</li>
+          <li><Icon name="calendar" />{book.date}</li>
+          <li>
+            <Icon name="file text" />
+            <span className="item">{book.page} pages</span>
+            <Icon name={book.category === 'React' ? 'react' : 'js'} />
+            <span>{book.category}</span>
+          </li>
+          <li><strong>ISBN</strong> : {book.isbn}</li>
+          <li className="text">{book.accept}</li>
+          <li>
+            <Link to={`/books/${book.id}`} className="more">Learn More</Link>
+            <button
+              className="delete"
+              type="button"
+              onClick={() => deleteBook(book.id)}
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
+      </section>
     )
   }
 }
